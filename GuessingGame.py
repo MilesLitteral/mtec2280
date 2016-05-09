ff652340d4e2918f09a9ab6066d7f9d91be1cb0f
SasoriZero <Mandaloe2@gmail.com>
2016-05-09 01:05:43 -0400

    Guessing Game
    Added Guessing Game Assignment

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,61 +1,34 @@
-#!/usr/bin python
-import sys
+def guessGame():
+	guesses = ["Pigs", "Angeles", "Robot"]
+	questions = ["____ in a blanket", "Los _______", "Super _____ Wars"]
+	tries = 3
+	success = 0
 
-#sign =  input("input a sight")
-#operandA = input("First Operand")
-#operandB = input("Second Operand")
-trivia = {'Question1' : 10, 'Question2' : 25, 'Question3' : 3,  'Question4' : 25 }
+	print("Welcome to Miles' Guessing Game!")
+	print("Starting Game")
+	playGame = True
+	q = 0
+	x = 0
+	while(playGame == True):
+		if(success >= 3):
+			print("Congradulations! You Won!")
+			return 0
+			
+		if(tries <= 0):
+			print("Game Over")
+			playGame = False
+			return 0
 
-class Score:
+		print(questions[q])
+		answer = raw_input("Answer: ")
 
-	def __init__(self, s):
-		self.score = s
+		if(str(answer) == guesses[x]):
+			success = success + 1
+			q = q + 1
+			x = x + 1
+		elif(str(answer) != guesses[x]):
+			tries = tries - 1
+			print("Try again, tries remaining %i" % tries)
 
-	def setScore(self, pts):
-		self.score += pts
 
-	def calculateScore(self):
-		if (self.score >= 4 or self.score > 2):
-			print ("Final Score is: ")
-			print(self.score)
-			print("You Win!")
-		elif(selfself.score < 2):
-			print("You Lose!")
-
-def main():
-	sc = Score(0)
-
-	uInput = input("What is 5 + 5")
-	if (int(uInput) == trivia['Question1']):
-		sc.setScore(1)
-		print ("Correct")
-	else:
-		sc.setScore(0)
-		print("Incorrect")
-
-	uInput = input("What is 5 * 5")
-	if (int(uInput) == trivia['Question2']):
-		sc.setScore(1)
-		print ("Correct")
-	else:
-		sc.setScore(0)
-		print("Incorrect")
-
-	uInput = input("What is 15 / 5")
-	if (int(uInput) == trivia['Question3']):
-		sc.setScore(1)
-		print ("Correct")
-	else:
-		sc.setScore(0)
-		print("Incorrect")
-
-	uInput = input("What is 5 Squared")
-	if (int(uInput) == trivia['Question4']):
-		sc.setScore(1)
-		print ("Correct")
-		sc.calculateScore()
-	else:
-		sc.setScore(0)
-		print("Incorrect")
-
-main()
+guessGame()
