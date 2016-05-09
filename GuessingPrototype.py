8fb557581f8c4620c7d0849eabab9033cbf37058
SasoriZero <Mandaloe2@gmail.com>
2016-05-09 01:21:52 -0400

    Prototype Guessing Game and PyCalculator
    Python Calculator and first Guessing Game assignment

---
+++ GuessingPrototype.py
@@ -0,0 +1,7 @@
+print("Guessing Prototype Game!")
+x = raw_input("What is the answer to life, the universe, and everything?")
+
+if "42" in x:
+	print("Great Guess!")
+else:
+	print("Meditation this requires")
