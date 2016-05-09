2831bf67277f7dbf79924e425f391bbce41986e6
SasoriZero <Mandaloe2@gmail.com>
2016-05-09 11:19:38 -0400

    JumpNDie May Version
    Current Version of JumpNDie

--- JumpnDie.py
+++ JumpnDie.py
@@ -14,6 +14,7 @@ def isDead(inputB):
 		reduceJumps(inputB)
 		return False
 	if(inputB == 0):
+		#SoundEffect - DEATH
 		print("You Died")
 		return True
 
@@ -22,25 +23,87 @@ def resetJumps(inputA):
 
 def reduceJumps(inputC):
 	inputC -= 1
+	#SoundEffect - JUMPDOWN
 	return inputC
 
 def Rewards(Jumper, RandomEvent):
 	rwn = RNG()
 	if(rwn == 0):
+		#SoundEffect - REST
 		print(RandomEvent['Rest'])
 		Jumper = GlobalJump
 		return Jumper
 	if(rwn == 1):
+		#SoundEffect - Full Restore
 		print(RandomEvent['RestoreA'])
 		Jumper = Jumper + 1
 		return Jumper
 	if(rwn == 2):
+		#SoundEffect - Bad Luck
 		print(RandomEvent['BadLuck'])
 		Jumper = Jumper - 1
 		return Jumper
 	else:
+		#SoundEffect - DEATH
 		print("No Rewards, Just a case of Bad Luck")
 		return Jumper
+
+def ClockBoost(value, clock):
+	value = value + 2;
+	print("Boost Value: %s" % value)
+	clock = clock + value
+	return clock 
+
+
+def nextLevel(lvl):
+	if(lvl == 0):
+		LevelOne
+	elif(lvl == 1):
+
+
+	elif(lvl == 2):
+
+
+def RabbitHole(jp, SLegs):
+	RH = {'Diamonds' : "You are hit repeatedly with Arrows!", 'Hearts' : "You continue falling. . . . . . . . . .", 'Spades' : "You hit the floor", 'Clubs' : "You hit the ground, realizing your legs are stone!"}
+	rwn = RNG() % 4
+	
+	if(rwn == 0):
+		print("You fall. . .")
+		time.sleep(2.5)
+		print("You hear slithering, an ominous voice speaks")
+		time.sleep(1.5)
+		print("I will weigh your actions, justice or punishment")
+		time.sleep(1.5)
+		rt = RNG() % 2
+		if(rt == 1 or rt == 0):
+			print("Justice")
+			time.sleep(1.0)
+			jp = jp + 1
+		if(rt == 2):
+			print("Punishment")
+			time.sleep(2.5)
+			jp = jp - 1
+		print("You realize you are in place, never having moved at all")
+		time.sleep(2.5)
+
+	if(rwn == 1):
+		print(RH['Diamonds'])
+		jp = jp - 3
+
+	if(rwn == 2):
+		print(RH['Hearts'])
+		SLegs = True
+		nextLevel()
+
+	if(rwn == 3):
+		print(RH['Spades'])
+
+	if(rwn == 4):
+		print(RH['Clubs'])
+		print("You then trampolene, back to your original position")
+		print("Your legs ache from hitting the ground")
+		jp = jp - 1
 
 def fightWyvern(jumps):
 	PlayerStates = {'Rest' : 0, 'Attack' : 1}
@@ -61,12 +124,14 @@ def fightWyvern(jumps):
 			return True
 
 		if(Jumps <= 0):
+			#SoundEffect - DEATH
 			print("Game Over")
 			aPlayer = False
 			GlobalTruth = False
 			return False
 
 		if(cPlayerState == PlayerStates['Attack']):
+				#SoundEffect - DEATH
 				print("It's your Turn! Attack?")
 				userInputB = input("(1/0)")
 				
@@ -74,6 +139,7 @@ def fightWyvern(jumps):
 					rdmroll = RNG()
 
 					if(rdmroll == 0):
+						#SoundEffect - DEATH
 						print("Missed Wyvern !")
 						print("Jumps: %s remaining" % Jumps)
 						print("\n")
@@ -84,6 +150,7 @@ def fightWyvern(jumps):
 					if(rdmroll == 1):
 						WyvernHP -= 1
 						Jumps -= 1
+						#SoundEffect - DEATH
 						print("Hit Wyvern !")
 						print("Jumps: %s remaining" % Jumps)
 						print("Wyvern Health: %s remaining" % WyvernHP)
@@ -95,6 +162,7 @@ def fightWyvern(jumps):
 
 					if(rdmroll == 2):
 						WyvernHP -= 3
+						#SoundEffect - DEATH
 						print("Critical Hit on Wyvern !")
 						print("Wyvern Health: %s remaining" % WyvernHP)
 						print("\n")
@@ -106,6 +174,7 @@ def fightWyvern(jumps):
 					if(rdmroll == 3):
 						WyvernHP -= 1
 						Jumps -= 1
+						#SoundEffect - DEATH
 						print("Hit Wyvern !")
 						print("Jumps: %s remaining" % Jumps)
 						print("Wyvern Health: %s remaining" % WyvernHP)
@@ -117,6 +186,7 @@ def fightWyvern(jumps):
 
 					if(rdmroll == 4):
 						WyvernHP -= 3
+						#SoundEffect - DEATH
 						print("Critical Hit on Wyvern !")
 						print("Wyvern Health: %s remaining" % WyvernHP)
 						print("\n")
@@ -126,6 +196,7 @@ def fightWyvern(jumps):
 						cWyvernState = WyvernStates['Attack']
 
 					if(rdmroll == 5):
+						#SoundEffect - DEATH
 						print("Missed Wyvern !")
 						print("Jumps: %s remaining" % Jumps)
 						print("\n")
@@ -146,6 +217,7 @@ def fightWyvern(jumps):
 			rdmroll = RNG()
 
 			if(rdmroll == 0):
+				#SoundEffect - DEATH
 				print("Wyvern missed!")
 				print("Jumps: %s remaining" % Jumps)
 				print("Wyvern Health: %s remaining" % WyvernHP)
@@ -154,6 +226,7 @@ def fightWyvern(jumps):
 				cPlayerState = PlayerStates['Attack']
 				cWyvernState = WyvernStates['Rest']
 			if(rdmroll == 1):
+				#SoundEffect - DEATH
 				print("Wyvern hit!")
 				Jumps -= 1
 				print("Jumps: %s remaining" % Jumps)
@@ -163,6 +236,7 @@ def fightWyvern(jumps):
 				cPlayerState = PlayerStates['Attack']
 				cWyvernState = WyvernStates['Rest']
 			if(rdmroll == 2):
+				#SoundEffect - DEATH
 				print("Wyvern missed!")
 				print("Jumps: %s remaining" % Jumps)
 				print("Wyvern Health: %s remaining" % WyvernHP)
@@ -171,12 +245,14 @@ def fightWyvern(jumps):
 				cPlayerState = PlayerStates['Attack']
 				cWyvernState = WyvernStates['Rest']
 			if(rdmroll == 3):
+				#SoundEffect - DEATH
 				print("Wyvern missed!")
 				print("Jumps: %s remaining" % Jumps)
 				print("Wyvern Health: %s remaining" % WyvernHP)
 				print("\n")
 
 			if(rdmroll == 4):
+				#SoundEffect - DEATH
 				print("Critical hit!")
 				Jumps -= 4
 				print("Jumps: %s remaining" % Jumps)
@@ -186,6 +262,7 @@ def fightWyvern(jumps):
 				cPlayerState = PlayerStates['Attack']
 				cWyvernState = WyvernStates['Rest']
 			if(rdmroll == 5):
+				#SoundEffect - DEATH
 				print("Wyvern Grazed!")
 				Jumps -= 1
 				print("Jumps: %s remaining" % Jumps)
@@ -215,11 +292,12 @@ def secondLevel():
 	while(on == True):
 		if(terminus >= 25):
 			print("You Win!")
-			on = false
+			on = False
 
 		rdn = RNG()
 
 		if(rdn == 0):
+			#SoundEffect - DEATH
 			print(LevelTwo['ChallengeA'])
 			print("Jumps: %s remaining" % SJumps)
 			print("Adversity: %s Passed" % terminus)
@@ -243,6 +321,7 @@ def secondLevel():
 				continue
 
 		if(rdn == 1):
+			#SoundEffect - DEATH
 			print(LevelTwo['ChallengeB'])
 			print("Jumps: %s remaining" % SJumps)
 			
@@ -271,16 +350,19 @@ def secondLevel():
 				on = False
 
 		if(rdn == 2):
+			#SoundEffect - DEATH
 			print("You're walking")
 			time.sleep(2.5)
 
 		if(rdn == 3):
+			#SoundEffect - DEATH
 			print(LevelTwo['ChallengeC'])
 			
 			if(SJumps < 1):
 				print("You Died")
 				on = False
 			else:
+				#SoundEffect - DEATH
 				print("Adversity: %s Passed" % terminus)
 				print("Adversity: %s Remaining" % (goal - terminus))
 				print("Jumps: %s remaining" % SJumps)
@@ -290,10 +372,12 @@ def secondLevel():
 				time.sleep(2.5)
 
 		if(rdn == 4):
+			#SoundEffect - DEATH
 			SJumps = Rewards(int(SJumps), RandomEvents)
 			time.sleep(2.5)
 
 		if(rdn == 5):
+			#SoundEffect - DEATH
 			print(LevelTwo['ChallengeD'])
 			terminus = terminus + 1
 			print("Adversity: %s Passed" % terminus)
@@ -308,6 +392,383 @@ def secondLevel():
 		else:
 			print("You're Walking, yet still going backwards")
 			time.sleep(2.5)
+
+def levelThree():
+	LevelTwo = {'ChallengeA' : "Stalagtite! Jump out of the way!", 'ChallengeB' : "A Doomba appears, do you jump on it", 'ChallengeC' : "Pitfall!"}
+	RandomEvents = {'Rest': "Your legs feel rested", 'RestoreA': "You can jump one more time",'Power': "Hit a Power Block! Temporary infinite jumps", 'BadLuck' : "You lost one jump"}
+	
+	on = True
+	stoneLegs = False;
+	terminus = 0
+	goal = 25
+	SJumps = 15
+	ceilingClock = 30
+	GlobalJump = SJumps
+
+	print("The Sky is falling")
+	print("Run")
+	print("\n")
+	time.sleep(2.5)
+
+	while(on == True):
+		if(ceilingClock <= 0):
+			print("Game Over, You were crushed by divine wrath")
+			on = False
+			break
+
+		if(terminus >= 25):
+			print("You Win!")
+			on = False
+
+		rdn = RNG()
+
+		if(rdn == 0):
+			ceilingClock = ClockBoost(RNG(),ceilingClock)
+			print("Ceiling Time boosted: %s " % ceilingClock)
+			time.sleep(2.5)
+
+		if(rdn == 1):
+			ceilingClock = ceilingClock - 1
+			#SoundEffect - STALAGTITE
+			print(LevelTwo['ChallengeA'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				terminus = terminus + 1
+				ceilingClock = ceilingClock - 1
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("Ceiling Time remaining: %s" % ceilingClock)
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 2):
+			#SoundEffect - DOOMBA
+			ceilingClock = ceilingClock - 1
+			print(LevelTwo['ChallengeB'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				ceilingClock = ceilingClock - 1
+				terminus = terminus + 1
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("Ceiling Time remaining: %s" % ceilingClock)
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 3):
+			#SoundEffect - PITTFALL
+			ceilingClock = ceilingClock - 1
+			print(LevelTwo['ChallengeC'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				ceilingClock = ceilingClock - 1
+				terminus = terminus + 1
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("Ceiling Time remaining: %s" % ceilingClock)
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 4):
+			ceilingClock = ceilingClock - 1
+			RabbitHole(SJumps,False)
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			time.sleep(2.5)
+			
+
+		if(rdn == 5):
+			ceilingClock = ceilingClock - 1
+			SJumps = Rewards(int(SJumps), RandomEvents)
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			time.sleep(2.5)
+
+
+
+
+def levelFour():
+	LevelTwo = {'ChallengeA' : "Stalagmite! Jump out of the way!", 'ChallengeB' : "A Doomba appears, do you jump on it", 'ChallengeC' : "Pitfall!", 'ChallengeD' : "A Wild Wyvern Appears!"}
+	RandomEvents = {'Rest': "Your legs feel rested", 'RestoreA': "You can jump one more time",'Power': "Hit a Power Block! Temporary infinite jumps", 'BadLuck' : "You lost one jump"}
+	
+	on = True
+	terminus = 0
+	goal = 25
+	SJumps = 15
+	GlobalJump = SJumps
+
+	print("Salvation Lies near, Stay strong")
+	print("\n")
+	time.sleep(2.5)
+
+
+	while(on == True):
+		if(terminus >= 25):
+			print("You Win!")
+			on = False
+
+		rdn = RNG()
+		if(rdn == 0):
+			time.sleep(2.5)
+
+		if(rdn == 1):
+			#SoundEffect - STALAGTITE
+			print(LevelTwo['ChallengeA'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				terminus = terminus + 1
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 2):
+			#SoundEffect - DOOMBA
+			print(LevelTwo['ChallengeB'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 3):
+			#SoundEffect - PITTFALL
+			print(LevelTwo['ChallengeC'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 4):
+			RabbitHole(SJumps,False)
+			time.sleep(2.5)
+			
+		if(rdn == 5):
+			SJumps = Rewards(int(SJumps), RandomEvents)
+			time.sleep(2.5)
+
+		if(terminus >= 25):
+			print("You Win!")
+			on = false
+
+		rdn = RNG()
+
+
+def levelFive():
+	LevelTwo = {'ChallengeA' : "Stalagmite! Jump out of the way!", 'ChallengeB' : "A Doomba appears, do you jump on it", 'ChallengeC' : "Pitfall!", 'ChallengeD' : "A Wild Wyvern Appears!"}
+	RandomEvents = {'Rest': "Your legs feel rested", 'RestoreA': "You can jump one more time",'Power': "Hit a Power Block! Temporary infinite jumps", 'BadLuck' : "You lost one jump"}
+	
+	on = True
+	terminus = 0
+	goal = 25
+	SJumps = 15
+	GlobalJump = SJumps
+
+	print("It Ends")
+	print("\n")
+	time.sleep(2.5)
+
+	while(on == True):
+		if(terminus >= 25):
+			print("You Win!")
+			on = False
+
+		rdn = RNG()
+
+		if(rdn == 0):
+			ceilingClock = ClockBoost(RNG(),ceilingClock)
+			print("Ceiling Time boosted: %s " % ceilingClock)
+			time.sleep(2.5)
+
+		if(rdn == 1):
+			#SoundEffect - STALAGTITE
+			print(LevelTwo['ChallengeA'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				terminus = terminus + 1
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("Ceiling Time remaining: %s" % ceilingClock)
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 2):
+			#SoundEffect - DOOMBA
+			print(LevelTwo['ChallengeB'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				terminus = terminus + 1
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("Ceiling Time remaining: %s" % ceilingClock)
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 3):
+			#SoundEffect - PITTFALL
+			print(LevelTwo['ChallengeC'])
+			print("Jumps: %s remaining" % SJumps)
+			print("Adversity: %s Passed" % terminus)
+			print("Adversity: %s Remaining" % (goal - terminus))
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			print("\n")
+
+			if(SJumps < 1):
+				print("You Died")
+				on = False
+			else:
+				userInputB = input("(1/0)")
+				time.sleep(2.5)
+				SJumps = reduceJumps(SJumps)
+				Dead = isDead(userInputB)
+
+			if(Dead == False):
+				terminus = terminus + 1
+				print("Adversity: %s Passed" % terminus)
+				print("Adversity: %s Remaining" % (goal - terminus))
+				print("Ceiling Time remaining: %s" % ceilingClock)
+				print("\n")
+				time.sleep(2.5)
+				continue
+
+		if(rdn == 4):
+			RabbitHole(SJumps,False)
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			time.sleep(2.5)
+			
+
+		if(rdn == 5):
+			SJumps = Rewards(int(SJumps), RandomEvents)
+			print("Ceiling Time remaining: %s" % ceilingClock)
+			time.sleep(2.5)
+			
+		if(terminus >= 25):
+			print("You Win!")
+			on = false
+
+		rdn = RNG()
+
+def Credits():
+	cred = {}
 
 def main():
 	LevelOne = {'ChallengeA' : "There is a pipe ahead, would you like to enter?", 'ChallengeB' : "A Doomba appears, do you jump on it", 'ChallengeC' : "There's Lava!"}
@@ -421,5 +882,24 @@ def main():
 	elif(int(userInput) == 0):
 		print("Maybe some other time")
 		on = False
+
+	elif(int(userInput) == 3):
+		print("DEBUG LEVEL SELECTOR")
+		print("Valid Inputs: 1, 2, 3, 4, 5")
+		uimp = input("Input a Level Value")
+		if(uimp == 1):
+			LevelOne()
+		elif(uimp == 2):
+			LevelTwo()
+
+		elif(uimp == 3):
+			levelThree()
+
+		elif(uimp == 4):
+			levelFour()
+
+		elif(uimp == 5):
+			levelFive()
+
 
 main()
